1227a34bc438ab17f3fa4a408f97ae1a9d853d6a
Aleksander Waage <alek.ntnu@gmail.com>
2020-11-23 19:44:26 +0100

    v1

---
+++ OWML.Cammie/Cammie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using OWML.Common;
+using OWML.ModHelper;
+using UnityEngine;
+
+namespace Cammie
+{
+    public class Cammie : ModBehaviour
+    {
+        private string _folder;
+        private int _size;
+
+        public override void Configure(IModConfig config)
+        {
+            _size = config.GetSettingsValue<int>("size");
+            if (_size < 1)
+            {
+                _size = 1;
+            }
+            ModHelper.Console.WriteLine($"Set size to {_size}");
+        }
+
+        private void Start()
+        {
+            _folder = $"{ModHelper.Manifest.ModFolderPath}/Screenshots";
+
+            if (!Directory.Exists(_folder))
+            {
+                Directory.CreateDirectory(_folder);
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.P))
+            {
+                var path = $"{_folder}/Screenshot-{DateTime.Now:yyyy-MM-dd-HH-mm-ss-ffff}.png";
+                ModHelper.Console.WriteLine($"Taking screenshot {path}");
+                ScreenCapture.CaptureScreenshot(path, _size);
+            }
+        }
+    }
+}
